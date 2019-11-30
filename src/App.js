687f562c4fcfc4e8dca7a2385922dfe64a2a6404
OlgaTkachuk <tkachukk.olga@gmail.com>
2019-12-01 00:13:15 +0200

    trying to fix bug[3]

--- src/App.js
+++ src/App.js
@@ -15,33 +15,39 @@ import Error from "./pages/Error";
 import {Route, Switch} from 'react-router-dom'
 import {LanguageConsumer, LanguageProvider} from "./Components/LanguageContext";
 import {Helmet} from 'react-helmet';
+
 class App extends Component {
 
     render() {
         return (
-                <LanguageProvider>
-                    <LanguageConsumer>
-                        {({lang = ''}) => (
-                            <div className={lang == "en" ? "latin-font" : "cyrillic-font"}>
-                                <Helmet title={lang == "en" ? "TOR Glass | Blown glass producer": "TOR Glass | Вироби зі скла"}
-                                        meta={[
-                                            {"name": "description", "content": lang == "en" ? "Producer of glass artistic items and also technical colored glass. We specialize on art glassware in a wide range of shapes, designs, colours and sixes." : "Виробник скляних художніх предметів, а також технічного кольорового скла. Ми спеціалізуємось на художньому скляному посуді в широкому діапазоні форм, дизайну, кольорів та розмірів"},
-                                        ]}
-                                <Navbar/>
-                                <Switch>
-                                    <Route exact path="/" component={Home}/>
-                                    <Route exact path="/about" component={About}/>
-                                    <Route exact path="/catalog" component={Catalog}/>
-                                    <Route exact path="/collection/:slug" component={Collection}/>
-                                    <Route exact path="/custom" component={CustomMade}/>
-                                    <Route exact path="/news" component={WhatsNew}/>
-                                    <Route exact path="/contacts" component={Contacts}/>
-                                    <Route exact path="/dev" component={InDevelopment}/>
-                                    <Route component={Error}/>
-                                </Switch>
-                            </div>)}
-                    </LanguageConsumer>
-                </LanguageProvider>
+            <LanguageProvider>
+                <LanguageConsumer>
+                    {({lang = ''}) => (
+                        <div className={lang == "en" ? "latin-font" : "cyrillic-font"}>
+                            <Helmet
+                                title={lang == "en" ? "TOR Glass | Blown glass producer" : "TOR Glass | Вироби зі скла"}
+                                meta={[
+                                    {
+                                        "name": "description",
+                                        "content": lang == "en" ? "Producer of glass artistic items and also technical colored glass. We specialize on art glassware in a wide range of shapes, designs, colours and sixes." : "Виробник скляних художніх предметів, а також технічного кольорового скла. Ми спеціалізуємось на художньому скляному посуді в широкому діапазоні форм, дизайну, кольорів та розмірів"
+                                    },
+                                ]}
+                            />
+                            <Navbar/>
+                            <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route exact path="/about" component={About}/>
+                                <Route exact path="/catalog" component={Catalog}/>
+                                <Route exact path="/collection/:slug" component={Collection}/>
+                                <Route exact path="/custom" component={CustomMade}/>
+                                <Route exact path="/news" component={WhatsNew}/>
+                                <Route exact path="/contacts" component={Contacts}/>
+                                <Route exact path="/dev" component={InDevelopment}/>
+                                <Route component={Error}/>
+                            </Switch>
+                        </div>)}
+                </LanguageConsumer>
+            </LanguageProvider>
         );
     }
 }
